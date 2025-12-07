d4bf94c4c3fe1eb9935ca1a3db832cd37a3d7a7e
Pothula Ajay <149940757+Ajay-Pothula@users.noreply.github.com>
2025-12-07 11:11:32 +0530

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,7 +1,9 @@
+# For testing what all the models there that can be used....Not needed to run in runtime
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBriKnuIloYXW7ByzYPm0BZmQndhlj5C9Q")
+genai.configure(api_key="API_KEY")
 
 print("Listing models:")
 for m in genai.list_models():
     print(m.name)
+
